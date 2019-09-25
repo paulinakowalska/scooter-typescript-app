69d6b5fd9224bcc32c888d53b4841729d5833718
paulinakowalska <paulina.kowalska@schibsted.com>
2019-09-25 23:33:34 +0200

    Setup project: - eslint/prettier/stylelint/jest - lerna - babel and webpack - created basic component
    Add basic tsconfig file
    
    Fix eslint and stylelint setup
    
    Add babel and webpack config and create basic component

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+        },
+    },
+    rules: {
+        'react/prop-types': 'off',
+        'no-console': ['warn'],
+        'prettier/prettier': 'error',
+        'no-var-requires': 0,
+    },
+    env: {
+        node: true,
+        'jest/globals': true,
+        browser: true,
+    },
+    plugins: ['jest', 'prettier', 'react'],
+};
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    roots : ['<rootDir>/packages'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    setupTestFrameworkScriptFile: '<rootDir>jest/setupTests.ts'
+};
